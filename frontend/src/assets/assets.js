b7e5f34cdea3f604b81f1cbb6801779bc48b445f
Hey-Mr-Buddy <vikramaditya.i.528@gmail.com>
2026-02-23 11:30:54 +0530

    Image changed

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,5 +1,4 @@
 import logo from './logo.png'
-import hero_img from './hero_img.png'
 import cart_icon from './cart_icon.png'
 import bin_icon from './bin_icon.png'
 import dropdown_icon from './dropdown_icon.png'
@@ -19,7 +18,6 @@ import cross_icon from './cross_icon.png'
 
 export const assets = {
     logo,
-    hero_img,
     cart_icon,
     dropdown_icon,
     exchange_icon,
